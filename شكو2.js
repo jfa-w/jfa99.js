467bbc3a0af623be1cb1ed94138baefb5d6e2354
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-11-05 16:21:57 +0300

    Create شكو2.js

---
+++ شكو2.js
@@ -0,0 +1,19 @@
+try{
+														$(`<a id="عاصم" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a><a id="asim27" target="_blank" type="a" rel="nofollow" title="لتبوكون التصميم ولد الجحش" href="https://www.dookmobile.com" class="label-primary"></a><img id="asim70"src="https://iraqna.net/sico/1602857219172.png"style="width:100%"></div></div><center>`).insertBefore(".nav-tabs"),$(".nav-tabs").addClass("fl").css("width","100%"),
+$(`<center><div id="asim1">
+<a id="asim7"rel="dofollow" title="القوانين" href="https://iraqna.net/rules.html"style="border-radius: 0 4px 0 0; border-right: 2px solid#f7d322;"class="fl mini  fa fa-bullhorn btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="الاشتراكات" href="https://iraqna.net/sub.html"style="border-right: 1px solid#fff;"class="fl mini  fa fa-star-half-o btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="راديو" href="https://iraqna.net/radioiraqna.html"style="border-right: 1px solid#fff;"class="fl mini  fa fa-volume-up btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="زخرفة اسماء" href="https://blog.iraqna.net/z"style="border-right: 1px solid#fff;"class="fl mini  fa fa-gg btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اتصل بنا" href="https://iraqna.net/contact.html"style="border-right: 1px solid#fff;"class="fl mini fa fa-dot-circle-o btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="قوانين السوابر" href="https://iraqna.net/ruless.html"style="border-right: 1px solid#fff;"class="fl mini fa fa-exclamation-triangle btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="تطبيق الشات" href="https://iraqna.net/mobile.html"style="border-right: 1px solid#fff;"class="fl mini fa fa-android btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اختصارات الشات" href="https://iraqna.net/Shortcut.html"style="border-radius: 6px 0 0 0; border-left: 3px solid#f7d322; border-right: 1px solid#ffffff;"class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a>
+</div></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<center><div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="asim12">شــكــو مــاكــو </font> ● · ‫أفضل سوبر لهذا الأسبوع · ● <font class="asim11" color="#727A54">&nbsp; مـجـنـونـة &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="asim11" color="#ff9900">&nbsp;كـنـوش&nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="asim11" color="#ff00ff">&nbsp; جـمـيـلـة&nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="asim11" color="#5CAD9A">&nbsp; انـــس &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="asim11" color="#800000">&nbsp; مـهـره &nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ● <font class="asim11" color="#417570">&nbsp; ريـــاح &nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/jfa-w/jfa99.js/3a33754718ad00392627709076cc3bcb9d8f7b38/شكو.css"></style>`).insertBefore("body");
+}
+														catch (e){
+															console.log(e)
+														}

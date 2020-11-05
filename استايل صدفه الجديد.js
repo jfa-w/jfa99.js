8dc942626580a3fd9984966aab92a599e8ebbbb7
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-11-05 15:58:44 +0300

    Create استايل صدفه الجديد.js

---
+++ استايل صدفه الجديد.js
@@ -0,0 +1,20 @@
+try{
+														$(`<img src="pner.gif" class="fr" style="width:100%;margin-top: 0px;" >
+
+
+`).insertBefore('.nav-tabs');
+
+$('.u-msg.dots').removeClass('dots');
+$(".ubnr").remove();
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/jfa-w/jfa99.js/46713e85aae8ba6f59e8a77b607deb066717981c/jfa-w.net.css"></style>`).insertBefore("body");
+$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '1px solid #EDEDED',borderLeft: '1px solid #adadad',borderTop: '1px solid #EDEDED',borderBottom: '1px solid #EDEDED',paddingTop: '3px',borderRadius: '5px'});
+$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '3px solid #EDEDED',borderLeft: '3px solid #EDEDED',borderTop: '1px solid #EDEDED',borderBottom: '1px solid #EDEDED',paddingTop: '3px',borderRadius: '5px'});
+$('.fr.borderg a').text('دوك موبايل')
+setTimeout (function (){ $("<div class='fr borderrg' style='padding:2px;background-color:white;margin-right:4px;'>جميع الحقوق محفوظة لـ: <a href='https://www.jfa-w.net//'>شأت جفى</a></div>").insertAfter($(".loginstat"));  },1000);
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 0px; width: 0px;">');
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="https://www.sudfachat.com/sico/1580237645774.png" width="70%" style="">`)
+
+}
+														catch (e){
+															console.log(e)
+														}

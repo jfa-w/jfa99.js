58d23a963e622e0521862517a89d0d737e599c3a
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-11-30 18:01:10 +0300

    Update awtar-iq-jfa0.js

--- awtar-iq-jfa0.js
+++ awtar-iq-jfa0.js
@@ -101,4 +101,55 @@ label.ae.fa.chats.label.fa-comment.label-warning {
 
 </style>`).insertBefore("body")
 
+اكواد الوحه هنا 
+=========================================
+	try{
+														
+
+if(location.protocol!='https:'){location.protocol='https:';}
+
+$(`<img src="https://awtar-iq.com/sico/1606056328183.jpg" class="fr" style="width:100%;margin-top: 0px;" >
+
+
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;border-radius: 0px!important;width:70px;margin:228px 2px 2px;margin-left: 316px!important;" class="fl mini  fa   fa-heart-o btn btn-warning"> زخرفة </a>
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;border-radius: 15px 0px 0px 0px!important;width:70px;margin:199px 2px 2px;margin-left: 316px!important;" class="fl mini  fa   fa-phone btn btn-info"> الأداره </a>
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;border-radius: 0px 0px 0px 15px!important;width:70px;margin:257px 2px 2px;margin-left: 316px!important;" class="fl mini  fa  fa-paint-brush btn btn-danger"> التطبيق </a>
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:70px;margin:199px 2px 2px;" class="fl mini  fa   fa-star btn btn-success"> اشتراك </a>
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;border-radius: 0px!important;width:70px;margin:228px 2px 2px;" class="fl mini  fa   fa-image btn btn-warning"> رمزيات </a>
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;    border-radius: 0px 0px 15px 0px!important;width:70px;margin:257px 2px 2px;" class="fl mini  fa fa-comment-o  btn btn-info"> شات </a>
+
+`).insertBefore('.nav-tabs');
+
+
+$.getScript("https://rawcdn.githack.com/jfa-w/jfa99.js/4e71fd33268e8d6de18db624b9d12311fba9be79/awtar-iq-jfa0.js");
+
+$(".fa-gear").text("الضبط").css("width","");
+
+$("[data-target=#wall]").attr('title',' متنفس الآقلأم و القلوب ')
+
+
+
+$("[data-target=#settings]").attr('title','     الأعدادات و الضبط')
+$("[data-target=#rooms]").attr('title','   الغرف و الرومـات ')
+$("[data-target=#chats]").attr('title','      المحادثات الخاصه  ')
+$("[data-target=#users]").attr('title','  ضيـوفنـا المتـواجدين  ')
+
+
+
+$(`<div><marquee direction="right" width="100%" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4"> اهلا وسهلا بكم في شات <font color="#357524" class="des2"> نجمة الحب للجوال </font>افضل سوبر لهذا الاسبوع<font class="des2" color="#800000"> قريبا </font>افضل زائر لهذا الاسبوع<font class="des2" color="#ff9900"> قريبا </font>افضل زائرة لهذا الاسبوع<font class="des2" color="#ff00ff"> قريبا 	 </font>مبدع الحائط لهذا الاسبوع<font class="des2" color="#5CAD9A"> قريبا </font>مبدعه الحائط لهذا الاسبوع<font class="des2" color="#800000"> َقريبا </font>مع تمنياتنا للجميع باطيب الاوقات </marquee></div>`).insertBefore('.lonline.light.break');
+
+(document.getElementById("tbox").placeholder = "(  عراقنا -واحد )");
+
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"> 
+<center><iframe src="https://mixlr.com/%D9%86%D9%88%D8%B1-%D8%B9%D9%85%D8%A7%D9%86/embed/?autoplay=true" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe></center>
+<center></center></center>
+</div>`);
+$('.fr.borderg a').text('دوك موبايل')
+$("a.label.label-primary.fl > img.fl").replaceWith('<img src="" class="fl" style="margin-right: 0px; width: 0px;">');$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="https://jfa-w.net/sico/1580237645774.png" width="70%" style="">`)
+
+}
+														catch (e){
+															console.log(e)
+														}
+
 													

aa5cb99ac50cdbc40e624849589457cb57684187
jfa-w <57237966+jfa-w@users.noreply.github.com>
2020-11-30 17:05:12 +0300

    Create awtar-iq-jfa.js

---
+++ awtar-iq-jfa.js
@@ -0,0 +1,114 @@
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/jfa-w/jfa99.js/6764bd83049eb6c16e8d2125010f45732d6a22e7/awtar-iq-jfa.css">
+
+<style>
+
+input#stealth {
+    margin-left: -17px;
+}
+
+div#tlogins img.fitimg.fl.u-pic {
+    border-radius: 5px 15px 5px 5px;
+}
+
+.border {
+    box-shadow: inset 1px 1px 1px 1px rgba(0,0,0,.075), 0 0 5px rgb(146 146 146 / 64%);
+    border: 1px solid white;
+}
+
+#des3 {
+    text-decoration: dotted;
+    color: #fff;
+    margin-left: 2px!important;
+    display: inline-block;
+    position: absolute;
+    margin-right: 4.4px!important;
+    background: #3b7db7;
+    background-image: url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Flag_of_Iraq.svg/220px-Flag_of_Iraq.svg.png);
+    background-blend-mode: luminosity;
+}
+
+a.label.label-primary.fl img.fl {display: none;}
+
+
+
+.fr.borderg, .fr.borderg a {
+    font-size: 10px!important;
+    padding: 0 5px!important;
+    position: relative;
+    color: #ffffff;
+}
+.fr.borderg {
+    border: 0px;
+    margin-top: 0;
+    height: 0;
+
+}
+
+
+label.label.label-primary.mini.fl {
+    padding: 1px;
+    border-radius: 15px!important;
+	margin-top: 1px;
+}
+
+
+.hmsg {background-color: rgb(235, 241, 255)!important;}
+#des1 {margin-bottom: 2px;font-family: 'El Messiri',FontAwesome!important;    background-image: linear-gradient(90deg,#d0d0d0 0,#bababa 10%,#908f8f 51%,#bababa 89%,#d0d0d0 100%);color: #f9f9f9;border-radius: 3px;padding: 4px 0!important;padding-bottom: 3px;}
+
+.ae.fa.label.label-primary.fa-user {
+    border-radius: 0 0 15px 0;
+}
+
+.ae.fa.chats.label.fa-comment.label-primary {
+    border-radius: 0 0 0 15px;
+}
+
+label.ae.fa.chats.label.fa-comment.label-warning {
+    border-radius: 0 0 0 15px;
+}
+
+.ae.fa.label.label-primary.fa-users {
+    border-radius: 0 0 15px 0;
+    margin-left: 10px!important;
+}
+
+.ae.fa.label.fa-commenting-o.label-warning {
+    border-radius: 0 0 0 15px;
+}
+
+.ae.label.label-primary.fa.fa-gear {
+    color: #ffd900;
+    border-radius: 0 0 15px 0;
+    margin-left: 10px!important;
+}
+
+label.ae.fa.label.fa-commenting-o.label-primary {
+    border-radius: 0px 0px 0px 15px;
+}
+
+#des0 {
+    margin: -134px 66px 2px;
+    border-radius: 10px 15px 1px 1px;
+    position: absolute;
+    width: 49% !important;
+    height: 19px;
+    left: -65px;
+    font-size: 14px !important;
+    color: #fff;
+    padding: 0px 0 !important;
+}
+
+.des2 {
+    background-color: #fff;
+    border-radius: 0;
+    padding: 2px 10px;
+    margin: 3px 8px!important;
+	border-radius: 15px!important;
+}
+
+</style>`).insertBefore("body")
+
+													}
+														catch (e){
+															console.log(e)
+														}
